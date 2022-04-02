8163ddb4c38e84563fd1d417fd46307f57feb1f9
luk4w <luk4w@outlook.com>
2022-04-02 15:40:26 -0300

    create window

--- main.cpp
+++ main.cpp
@@ -4,8 +4,31 @@
 using std::cout;
 using std::endl;
 
+const unsigned int WIDTH = 800; 
+const unsigned int HEIGHT = 600;
+
 int main()
 {
-    cout << "Hello Diario OpenGL!" << endl;
+    // Inicializar biblioteca GLFW
+    glfwInit();
+
+    // Instanciar GLFWwindow
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "DiarioOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Falha ao criar GLFWwindow!" << endl;
+        return -1;
+    }
+
+    // Loop de renderização principal
+    while (!glfwWindowShouldClose(window))
+    {
+        // Processar todos os eventos pendentes
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window); // Opcional
+    glfwTerminate(); // Terminar biblioteca GLFW
+
     return 0;
 } 
