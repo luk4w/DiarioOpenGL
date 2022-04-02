3115d0c7ced768115a36a0a0eaec4f925dfd25f7
luk4w <luk4w@outlook.com>
2022-04-02 18:47:57 -0300

    load OpenGL context

--- main.cpp
+++ main.cpp
@@ -13,6 +13,11 @@ int main()
     // Inicializar biblioteca GLFW
     glfwInit();
 
+    // Especificar versão do OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // Instanciar GLFWwindow
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "DiarioOpenGL", NULL, NULL);
     if (window == NULL)
@@ -20,10 +25,28 @@ int main()
         cout << "Falha ao criar GLFWwindow!" << endl;
         return -1;
     }
+    // Definir window como contexto atual da thread de chamada
+    glfwMakeContextCurrent(window);
+
+    // Verificar GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Nao foi possivel carregar o contexto OpenGL!" << std::endl;
+        return -1;
+    }
 
     // Loop de renderização principal
     while (!glfwWindowShouldClose(window))
     {
+        // Especificar os valores de vermelho, verde, azul e alfa, para limpar os buffers de cores
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // Definir qual ou quais buffers precisam ser limpos
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Trazer os "back buffers" para frente
+        glfwSwapBuffers(window);
+
         // Processar todos os eventos pendentes
         glfwPollEvents();
     }
