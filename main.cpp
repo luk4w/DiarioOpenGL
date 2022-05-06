70a4ef191dfaeff93169b9f0edf483e3459628d4
Luk4w <luk4w@outlook.com>
2022-05-06 14:12:55 -0300

    assign textures pointers

--- main.cpp
+++ main.cpp
@@ -93,13 +93,17 @@ int main()
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Atribuir ponteiros para os vertices
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     // Atribuir ponteiros para as cores
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3*sizeof(float)));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3*sizeof(float)));
     glEnableVertexAttribArray(1);
 
+    // Atribuir ponteiros para as texuturas
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+    glEnableVertexAttribArray(2);
+    
     // Desvincular VBO e VAO para não modificar acidentalmente
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
