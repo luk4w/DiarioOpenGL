ab0a5bcc469f77d422a74c00b6728419288f17be
Luk4w <luk4w@outlook.com>
2022-07-02 19:14:37 -0300

    move camera with mouse

--- main.cpp
+++ main.cpp
@@ -22,6 +22,13 @@ glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 3.0f);
 glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
 glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
+bool firstMouseInput = true;
+float yaw   = -90.0f;
+float pitch =  0.0f;
+float lastX =  800.0f / 2;
+float lastY =  600.0 / 2;
+float fov   =  45.0f;
+
 // Tempo entre o quadro atual e o último quadro
 float deltaTime = 0.0f;
 // Tempo do último quadro
@@ -187,7 +194,7 @@ int main()
     // Projection Matrix
     shaderProgram.use();
     glm::mat4 projection = glm::mat4(1.0f);  
-    projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+    projection = glm::perspective(glm::radians(fov), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
     shaderProgram.setMat4("projection", projection);
 
     // Loop de renderização principal
@@ -276,5 +283,37 @@ void processInput(GLFWwindow *window)
 
 void mouseCallback(GLFWwindow* window, double xposIn, double yposIn)
 {
-    
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouseInput)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouseInput = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos;
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
 }
