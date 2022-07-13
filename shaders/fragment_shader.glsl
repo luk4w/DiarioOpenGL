dfbfc00a9d496267522eae89ffd85652589b4ab9
Luk4w <luk4w@outlook.com>
2022-07-13 20:18:09 -0300

    rename textureCoordinate to TextureCoordinate

--- shaders/fragment_shader.glsl
+++ shaders/fragment_shader.glsl
@@ -1,11 +1,11 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 textureCoordinate;
+in vec2 TextureCoordinate;
 
 uniform sampler2D uniformTexture;
 
 void main()
 {
-    FragColor = texture(uniformTexture, textureCoordinate);
+    FragColor = texture(uniformTexture, TextureCoordinate);
 }
