d211e8037095fd56f6f43a9529f2ad9cc9d87899
Luk4w <luk4w@outlook.com>
2022-07-14 00:08:55 -0300

    remove texture

--- shaders/fragment_cube.glsl
+++ shaders/fragment_cube.glsl
@@ -1,11 +1,7 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec2 TextureCoordinate;
-
-uniform sampler2D uniformTexture;
-
 void main()
 {
-    FragColor = texture(uniformTexture, TextureCoordinate);
+    FragColor = vec4(1.0);
 }
