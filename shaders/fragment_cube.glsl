cdd2fecf848d5b5cb7cdb50f5053e78c91b4fd10
Luk4w <luk4w@outlook.com>
2022-07-14 02:49:44 -0300

    add diffuse lighting

--- shaders/fragment_cube.glsl
+++ shaders/fragment_cube.glsl
@@ -10,8 +10,19 @@ in vec3 FragPos;
 
 void main()
 {
+    // Iluminação ambiente
     float ambientLighting = 0.2;
+    vec3 ambient = ambientLighting * lightColor;
 
-    vec3 result = ambientLighting * lightColor * objectColor;
+    // Iluminação difusa
+    // normalize(u); calcula o vetor unitário na mesma direção do vetor original
+    vec3 norm = normalize(Normal);
+    vec3 lightDirection = normalize(lightPos - FragPos);
+    // max(-1, 2); retorna o maior valor
+    // dot(u, w); calcula o produto escalar de dois vetores
+    float diff = max(dot(norm, lightDirection), 0.0);
+    vec3 diffuse = diff * lightColor;
+
+    vec3 result = (ambient + diffuse) * objectColor;
     FragColor = vec4(result, 1.0);
 }
