6413eb2fd8361c3f52ec7a5fbdfc46df8dc91468
Luk4w <luk4w@outlook.com>
2022-07-14 20:33:50 -0300

    add setMat3

--- include/shader.h
+++ include/shader.h
@@ -17,6 +17,10 @@ public:
     {
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     };
+    void setMat3(const std::string &name, const glm::mat3 &mat) const
+    {
+        glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+    };
     void setVec3(const std::string &name, const glm::vec3 &value) const
     { 
         glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]); 
