166f197943f5334d9613ea43fed6a27bfd11a3f8
Luk4w <luk4w@outlook.com>
2022-07-16 15:56:25 -0300

    add struct Material

--- shaders/cube_fragment.glsl
+++ shaders/cube_fragment.glsl
@@ -1,7 +1,15 @@
 #version 330 core
 out vec4 FragColor;
 
-uniform vec3 objectColor;
+struct Material
+{
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float shininess;
+};
+uniform Material material;
+
 uniform vec3 lightColor;
 uniform vec3 lightPos; 
 uniform vec3 viewPos;
@@ -12,22 +20,20 @@ in vec3 FragPos;
 void main()
 {
     // Iluminação ambiente
-    float ambientLighting = 0.2;
-    vec3 ambient = ambientLighting * lightColor;
+    vec3 ambient = material.ambient * lightColor;
 
     // Iluminação difusa
     vec3 norm = normalize(Normal);
     vec3 lightDirection = normalize(lightPos - FragPos);
     float diff = max(dot(norm, lightDirection), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = (diff * material.diffuse) * lightColor;
 
     // Iluminação especular
-    float specularLighting = 0.5;
     vec3 viewDirection = normalize(viewPos - FragPos);
     vec3 reflectDirection = reflect(-lightDirection, norm);  
     float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), 32);
-    vec3 specular = specularLighting * spec * lightColor;  
+    vec3 specular = (material.specular * spec) * lightColor;  
 
-    vec3 result = (ambient + diffuse + specular) * objectColor;
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
