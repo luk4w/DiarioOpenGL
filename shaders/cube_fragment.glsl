1707213b099dd5c438242a3be348a98dc2320eb1
Luk4w <luk4w@outlook.com>
2022-07-22 20:00:46 -0300

    add texture

--- shaders/cube_fragment.glsl
+++ shaders/cube_fragment.glsl
@@ -3,8 +3,7 @@ out vec4 FragColor;
 
 struct Material
 {
-    vec3 ambient;
-    vec3 diffuse;
+    sampler2D diffuse;
     vec3 specular;
     float shininess;
 };
@@ -12,33 +11,34 @@ uniform Material material;
 
 struct Light
 {
+    vec3 position;
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
 };
 uniform Light light;
 
-uniform vec3 lightPos; 
 uniform vec3 viewPos;
 
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexturePos;
 
 void main()
 {
     // Iluminação ambiente
-    vec3 ambient = material.ambient * light.ambient;
+    vec3 ambient = light.ambient * texture(material.diffuse, TexturePos).rgb;
 
     // Iluminação difusa
     vec3 norm = normalize(Normal);
-    vec3 lightDirection = normalize(lightPos - FragPos);
+    vec3 lightDirection = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDirection), 0.0);
-    vec3 diffuse = (diff * material.diffuse) * light.diffuse;
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexturePos).rgb;  
 
     // Iluminação especular
     vec3 viewDirection = normalize(viewPos - FragPos);
     vec3 reflectDirection = reflect(-lightDirection, norm);  
-    float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), 32);
+    float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), material.shininess);
     vec3 specular = (material.specular * spec) * light.specular;  
 
     vec3 result = ambient + diffuse + specular;
