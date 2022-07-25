971d5c84f5404ed13b518662a03035b380530a03
Luk4w <luk4w@outlook.com>
2022-07-25 14:09:40 -0300

    change name of texture variable

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -2,7 +2,7 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexture;
 
-out vec2 TextureCoordinate;
+out vec2 TexCoords;
 
 uniform mat4 model;
 uniform mat4 view;
@@ -11,5 +11,5 @@ uniform mat4 projection;
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos, 1.0);
-    TextureCoordinate = vec2(aTexture.x, aTexture.y);
+    TexCoords = vec2(aTexture);
 }
