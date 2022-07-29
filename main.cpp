b4d2f3a6e35ab932657163cd0fb9aad4ed443fd4
Luk4w <luk4w@outlook.com>
2022-07-29 19:50:55 -0300

    add lamp positions

--- main.cpp
+++ main.cpp
@@ -78,8 +78,13 @@ int main()
     Model backpack("models/backpack/backpack.obj");
     Model cube("models/cube/cube.obj");
 
-    // Definir a posição da lâmpada
-    glm::vec3 lampPosition(4.0f, 0.0f, 0.0f);
+    glm::vec3 lampPositions[] =
+    {
+        glm::vec3( 0.7f, 0.2f, 2.0f),
+        glm::vec3( 2.3f, -3.3f, -4.0f),
+        glm::vec3(-4.0f, 2.0f, -12.0f),
+        glm::vec3( 0.0f, 0.0f, -3.0f)
+    };  
 
     // Loop de renderização principal
     while (!glfwWindowShouldClose(window))
