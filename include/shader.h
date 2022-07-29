1936a03781228007478aed143e8ee7b682931828
Luk4w <luk4w@outlook.com>
2022-07-28 23:21:26 -0300

    add setFloat

--- include/shader.h
+++ include/shader.h
@@ -13,6 +13,10 @@ public:
     unsigned int ID;
     Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr);
     void use();
+    void setFloat(const std::string &name, float value) const
+    { 
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value); 
+    }
     void setMat3(const std::string &name, const glm::mat3 &mat) const
     {
         glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
